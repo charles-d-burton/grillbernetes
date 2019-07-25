ca04b1df313e2d1428fed0eaf4fd347949cda96f
charles-d-burton <charles.burton@mindflash.com>
2019-07-24 22:10:48 -0600

    feat: added fibonacci backoff to control hub

--- control-hub/messagebus/messagebus.go
+++ control-hub/messagebus/messagebus.go
@@ -2,8 +2,11 @@ package messagebus
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
+	"time"
 
+	"github.com/jeffchao/backoff"
 	"github.com/nats-io/go-nats"
 	stan "github.com/nats-io/go-nats-streaming"
 )
@@ -19,25 +22,43 @@ type Message struct {
 }
 
 func Connect(host string) {
-	log.Println("Connecting to NATS at: ", host)
-	nc, err := nats.Connect(host)
-	if err != nil {
-		log.Fatal(err)
-	}
-	sc, err := stan.Connect("nats-streaming", "smoker-client", stan.NatsConn(nc))
-	if err != nil {
-		log.Fatal(err)
-	}
-	isConnected = true
-	log.Println("Conntected to NATS Streaming server: ", host)
+	f := backoff.Fibonacci()
+	f.Interval = 1 * time.Millisecond
+	f.MaxRetries = 20
+	cleanup := make(chan error, 1)
 	for {
-		select {
-		case message := <-pubs:
-			err := sc.Publish(message.Topic, message.Data)
+		connect := func() error {
+			log.Println("Connecting to NATS at: ", host)
+			nc, err := nats.Connect(host)
 			if err != nil {
-				log.Println(err)
+				return err
 			}
+			sc, err := stan.Connect("nats-streaming", "smoker-client", stan.NatsConn(nc),
+				stan.SetConnectionLostHandler(func(_ stan.Conn, reason error) {
+					log.Println(reason)
+					cleanup <- reason
+				}))
+			if err != nil {
+				return err
+			}
+			isConnected = true
+			log.Println("Conntected to NATS Streaming server: ", host)
+			select {
+			case message := <-pubs:
+				err := sc.Publish(message.Topic, message.Data)
+				if err != nil {
+					return err
+				}
+			case err := <-cleanup:
+				return err
+			}
+			return errors.New("An unknown error occured")
 		}
+		err := f.Retry(connect)
+		if err != nil {
+			log.Println(err)
+		}
+		f.Reset()
 	}
 }
 
--- go.mod
+++ go.mod
@@ -5,4 +5,9 @@ go 1.12
 require (
 	github.com/charles-d-burton/grillbernetes/control-hub v0.0.0-20190720212843-f294e2774799 // indirect
 	github.com/charles-d-burton/grillbernetes/pismoker v0.0.0-20190720212843-f294e2774799 // indirect
+	github.com/jeffchao/backoff v0.0.0-20140404060208-9d7fd7aa17f2 // indirect
+	github.com/nats-io/gnatsd v1.4.1 // indirect
+	github.com/nats-io/nats-server v1.4.1 // indirect
+	github.com/nats-io/nats-streaming-server v0.15.1 // indirect
+	github.com/prometheus/client_golang v1.0.0 // indirect
 )
